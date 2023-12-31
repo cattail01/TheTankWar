ba75b095e419841c61bb141b616111fb418f46a3
cattail <334750034@qq.com>
2023-12-31 22:11:25 +0800

    [assets] 书籍资源导入

--- Assets/HelloWorld/Scripts/MoveRightForever.cs
+++ Assets/HelloWorld/Scripts/MoveRightForever.cs
@@ -1,14 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace HelloWorld
 {
+    /// <summary>
+    /// [class] 一个让物体无限向y的代码，用于测试脚本是否可以正常运行
+    /// </summary>
     public class MoveRightForever : MonoBehaviour
     {
         // Update is called once per frame
         void Update()
         {
+            transform.Translate(0, 0.1f, 0);
         }
     }
 }
